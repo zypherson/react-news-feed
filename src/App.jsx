a05e0e44d6e3837003e6f36cbe57b8e4fc46981e
zypherson <taylorzypherson@gmail.com>
2025-10-30 02:31:57 -0400

    initated app skeleton

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
