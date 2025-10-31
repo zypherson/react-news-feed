d28bd96139247e122b1669ff8cb69d31a0c4e3a0
zypherson <taylorzypherson@gmail.com>
2025-10-31 12:54:26 -0400

    added homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Home.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Top Headlines</h2>
+      <p>News will appear here soon...</p>
+    </div>
+  );
+}
